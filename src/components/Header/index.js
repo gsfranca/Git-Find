3cbb6b0e2d5ad584a93975a5653682ae7fb83dfa
gsfranca <giovanasantosdefranca634@gmail.com>
2024-01-21 22:59:08 -0300

    unique commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+const Header = () => 
+{
+  return (
+    <header>
+        <h1>GitFind</h1>
+    </header>
+  )
+}
+
+export { Header }
---
+++ src/components/ItemList/index.js
@@ -0,0 +1,17 @@
+import "./styles.css"
+
+function ItemList( {title, description}) 
+{
+  return (
+    <div className="itemList">
+        
+        <strong>{title}</strong>
+        <p>{description}</p>
+
+        <hr />
+
+    </div>
+  )
+}
+
+export default ItemList
